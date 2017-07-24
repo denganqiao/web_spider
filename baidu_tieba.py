8280430aaaa745232f1097c46b0695888a6128a8
jfzhang95 <jfzhang@whu.edu.cn>
2017-07-24 23:15:23 +0800

    update

--- baidu_tieba.py
+++ baidu_tieba.py
@@ -1,21 +1,71 @@
-# -*- coding: utf-8 -*-
-# first web_spider
-
-import string
+# -*- coding:utf-8 -*-
+import urllib
 from urllib import request
+from bs4 import BeautifulSoup
+import re
+import json
+import requests
+from selenium import webdriver
+import random
 
-def baidu_tb(url, begin_page, end_page):
-    for i in range(begin_page, end_page+1):
-        sName = string.zfill(i, 5) + '.html'
-        print('正在下载第' + str(i) + '个网页，并将其存储为' + sName + '.....')
-        f = open(sName, 'w+')
-        m = request.urlopen(url+str(i)).read()
-        f.write(m)
-        f.close()
+# 这里填写无忧代理IP提供的API订单号（请到用户中心获取）
+order = "34145db9b4b4500e4ba4bf3b7865e38a"
+# 获取IP的API接口
+apiUrl = "http://api.ip.data5u.com/dynamic/get.html?order=" + order
+# 要抓取的目标网站地址
+url = "http://www.dianping.com/shop/16789967"
 
-bdurl = str(input(u'请输入贴吧地址：'))
-begin_page = int(input(u'请输入开始的页数：'))
-end_page = int(input(u'请输入终点的页数：'))
+# 获取IP列表
+res = request.urlopen(apiUrl).read().decode('utf-8').strip('\n')
+
+# 按照\n分割获取到的IP
+ips = res.split("\n")
+# 随机选择一个IP
+proxyip = random.choice(ips)
+
+proxy = {}
+proxy['http'] = proxyip
+
+# 使用代理IP请求目标网址
+proxy_support = request.ProxyHandler(proxy)
+opener = request.build_opener(proxy_support)
+request.install_opener(opener)
+
+my_headers = [
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.48"
+]
+
+random_header = random.choice(my_headers)
+
+# 进入浏览器设置
+options = webdriver.ChromeOptions()
+randdom_header = random.choice(random_header)
+# 更换头部
+options.add_argument('user-agent=my_headers')
+options.add_argument('Host="www.dianping.com"')
+
+options.add_argument('--proxy-server=%s' % proxyip)
+chrome_options = webdriver.ChromeOptions()
+browser = webdriver.Chrome(chrome_options=options)
+browser.get(url)
 
 
-baidu_tb(bdurl, begin_page, end_page)
+html = browser.page_source.decode('utf-8')
